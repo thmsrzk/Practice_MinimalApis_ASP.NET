c69e8b71124f0529eab986a285975284c6a8ed82
Thomas <thoms.rzk@gmail.com>
2024-08-27 20:30:38 +0200

    2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
 using MinimalApis;
 
 var builder = WebApplication.CreateBuilder();
+
+var list = new List<Article>
+{
+    new Article(1, "Marteau"),
+    new Article(2, "Scie")
+};
 
 var app = builder.Build();
 
@@ -13,6 +20,21 @@ app.MapPatch("/patch", () => "Hello PATCH!");
 app.MapMethods("/methods", new[] { "GET", "POST" }, () => "Hello methods!");
 
 app.MapGet("/article", () => new Article(1, "Marteau"));
-app.MapGet("/articles/{id}", (int id) => new Article(id, "Marteau"));
+
+app.MapGet("/articles/{id}", (int id) =>
+{
+    var article = list.Find(a => a.Id == id);
+    if (article != null) return Results.Ok(article);
+
+    return Results.NotFound();
+});
+
+app.MapGet("/personne/{nom}", (
+    [FromRoute] string nom,
+    [FromQuery] string? prenom,
+    [FromHeader(Name = "Accept-Encoding")] string encoding) => Results.Ok($"{nom} {prenom}"));
+
+//app.MapGet("/personne/identite", (Personne p) => Results.Ok(p));
+app.MapPost("/personne/identite", (Personne p) => Results.Ok(p));
 
 app.Run();
--- obj/Debug/net8.0/MinimalApis.AssemblyInfo.cs
+++ obj/Debug/net8.0/MinimalApis.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimalApis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41f652aeffa6473ddae90c36ec8421aa73a6b562")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85470dc09a2d069e50c0a346ba74d0990b556c7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimalApis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimalApis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
